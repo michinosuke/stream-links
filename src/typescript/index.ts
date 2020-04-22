93f018c19be81cabec81bb7b817cd8badc406ad1
michinosuke <michiya.318.3738@gmail.com>
2020-04-22 12:48:23 +0900

    first commit

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,3 +1,16 @@
-import d from './default'
+import d from './default';
 
 d();
+
+fetch('http://192.168.0.12:3000/data.json')
+  .then((res) => res.json())
+  .then((json) => {
+    for (let c = 1; c <= 5; c += 1) {
+      for (let r = 1; r <= 6; r += 1) {
+        const grid = document.querySelector(`.grid-${c}-${r}`);
+        grid.innerHTML = json[c][r].title;
+        grid.setAttribute('href', `https://web.microsoftstream.com/browse?q=%23${json[c][r].number}`);
+      }
+    }
+  })
+  .catch((error) => console.error(error));
